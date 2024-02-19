251303cc257921b1e6e01f4c05e4dcf731fdc1f7
Vinay Solanki <47470634+vinaysolanki535@users.noreply.github.com>
2024-02-19 15:18:01 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: [
       "oaidalleapiprodscus.blob.core.windows.net",
       "res.cloudinary.com",
     ],
-    output: "export",
   },
 };
 
